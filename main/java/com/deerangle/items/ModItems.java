21649be2f0275b11d52cd82597c249388c6b71b2
deerangle2 <ian.rehwinkel@kabelmail.de>
2017-12-08 14:59:53 +0100

    oof

--- main/java/com/deerangle/items/ModItems.java
+++ main/java/com/deerangle/items/ModItems.java
@@ -5,8 +5,8 @@ import net.minecraft.item.Item;
 
 public class ModItems {
 
-	public static Item schokoBarBlack;
 	public static Item schokoBarApple;
+	public static Item schokoBarBlack;
 	public static Item schokoBarCobble;
 	public static Item schokoBarCow;
 	public static Item schokoBarCreeper;
@@ -44,11 +44,73 @@ public class ModItems {
 	}
 
 	private static void initialize() {
+		schokoBarApple = new ItemSchokoBar("apple");
+		schokoBarBlack = new ItemSchokoBar("black");
+		schokoBarCobble = new ItemSchokoBar("cobble");
+		schokoBarCow = new ItemSchokoBar("cow");
+		schokoBarCreeper = new ItemSchokoBar("creeper");
+		schokoBarCristmas = new ItemSchokoBar("cristmas");
+		schokoBarDoor = new ItemSchokoBar("door");
+		schokoBarDrawing = new ItemSchokoBar("drawing");
+		schokoBarEnder = new ItemSchokoBar("ender");
+		schokoBarFire = new ItemSchokoBar("fire");
+		schokoBarFireworks = new ItemSchokoBar("fireworks");
+		schokoBarFlower = new ItemSchokoBar("flower");
+		schokoBarFull = new ItemSchokoBar("full");
+		schokoBarGlass = new ItemSchokoBar("glass");
+		schokoBarGold = new ItemSchokoBar("gold");
+		schokoBarHalloween = new ItemSchokoBar("halloween");
+		schokoBarIlluminati = new ItemSchokoBar("illuminati");
+		schokoBarJoghurt = new ItemSchokoBar("joghurt");
+		schokoBarLite = new ItemSchokoBar("lite");
+		schokoBarLSD = new ItemSchokoBar("lSD");
 		schokoBarNormal = new ItemSchokoBar("normal");
+		schokoBarNuts = new ItemSchokoBar("nuts");
+		schokoBarPortal = new ItemSchokoBar("portal");
+		schokoBarQuartz = new ItemSchokoBar("quartz");
+		schokoBarRedstone = new ItemSchokoBar("redstone");
+		schokoBarRubiks = new ItemSchokoBar("rubiks");
+		schokoBarSlot = new ItemSchokoBar("slot");
+		schokoBarSmartie = new ItemSchokoBar("smartie");
+		schokoBarSteve = new ItemSchokoBar("steve");
+		schokoBarWhite = new ItemSchokoBar("white");
+		schokoBarWood = new ItemSchokoBar("wood");
+		schokoBarYouTube = new ItemSchokoBar("youTube");
 	}
 
 	private static void register() {
+		GameRegistry.registerItem(schokoBarApple, "schokoBarApple");
+		GameRegistry.registerItem(schokoBarBlack, "schokoBarBlack");
+		GameRegistry.registerItem(schokoBarCobble, "schokoBarCobble");
+		GameRegistry.registerItem(schokoBarCow, "schokoBarCow");
+		GameRegistry.registerItem(schokoBarCreeper, "schokoBarCreeper");
+		GameRegistry.registerItem(schokoBarCristmas, "schokoBarCristmas");
+		GameRegistry.registerItem(schokoBarDoor, "schokoBarDoor");
+		GameRegistry.registerItem(schokoBarDrawing, "schokoBarDrawing");
+		GameRegistry.registerItem(schokoBarEnder, "schokoBarEnder");
+		GameRegistry.registerItem(schokoBarFire, "schokoBarFire");
+		GameRegistry.registerItem(schokoBarFireworks, "schokoBarFireworks");
+		GameRegistry.registerItem(schokoBarFlower, "schokoBarFlower");
+		GameRegistry.registerItem(schokoBarFull, "schokoBarFull");
+		GameRegistry.registerItem(schokoBarGlass, "schokoBarGlass");
+		GameRegistry.registerItem(schokoBarGold, "schokoBarGold");
+		GameRegistry.registerItem(schokoBarHalloween, "schokoBarHalloween");
+		GameRegistry.registerItem(schokoBarIlluminati, "schokoBarIlluminati");
+		GameRegistry.registerItem(schokoBarJoghurt, "schokoBarJoghurt");
+		GameRegistry.registerItem(schokoBarLite, "schokoBarLite");
+		GameRegistry.registerItem(schokoBarLSD, "schokoBarLSD");
 		GameRegistry.registerItem(schokoBarNormal, "schokoBarNormal");
+		GameRegistry.registerItem(schokoBarNuts, "schokoBarNuts");
+		GameRegistry.registerItem(schokoBarPortal, "schokoBarPortal");
+		GameRegistry.registerItem(schokoBarQuartz, "schokoBarQuartz");
+		GameRegistry.registerItem(schokoBarRedstone, "schokoBarRedstone");
+		GameRegistry.registerItem(schokoBarRubiks, "schokoBarRubiks");
+		GameRegistry.registerItem(schokoBarSlot, "schokoBarSlot");
+		GameRegistry.registerItem(schokoBarSmartie, "schokoBarSmartie");
+		GameRegistry.registerItem(schokoBarSteve, "schokoBarSteve");
+		GameRegistry.registerItem(schokoBarWhite, "schokoBarWhite");
+		GameRegistry.registerItem(schokoBarWood, "schokoBarWood");
+		GameRegistry.registerItem(schokoBarYouTube, "schokoBarYouTube");
 	}
 	
 }
