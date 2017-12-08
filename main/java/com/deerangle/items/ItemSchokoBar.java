4285db1faaadbad28cdbba361aecb5f7c82f5420
deerangle2 <ian.rehwinkel@kabelmail.de>
2017-12-08 14:35:16 +0100

    ok

--- main/java/com/deerangle/items/ItemSchokoBar.java
+++ main/java/com/deerangle/items/ItemSchokoBar.java
@@ -1,16 +1,37 @@
 package com.deerangle.items;
 
-import com.deerangle.main.ExampleMod;
+import com.deerangle.main.SchokoMod;
 
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
-public class ItemSchokoBar extends Item {
+public class ItemSchokoBar extends ItemFood {
 
-	public ItemSchokoBar(String type) {
+	public ItemSchokoBar(String type, int food) {
+		super(food, true);
 		type = type.substring(0, 1).toUpperCase() + type.substring(1);
 		this.setUnlocalizedName("schokoBar" + type);
-		this.setCreativeTab(ExampleMod.tab);
-		this.setTextureName(ExampleMod.MODID + ":schokoBar" + type);
+		this.setCreativeTab(SchokoMod.tab);
+		this.setTextureName(SchokoMod.MODID + ":schokoBar" + type);
+	}
+	
+	public ItemSchokoBar(String type) {
+		super(4, true);
+		type = type.substring(0, 1).toUpperCase() + type.substring(1);
+		this.setUnlocalizedName("schokoBar" + type);
+		this.setCreativeTab(SchokoMod.tab);
+		this.setTextureName(SchokoMod.MODID + ":schokoBar" + type);
+	}
+	
+	@Override
+	public ItemStack onEaten(ItemStack stack, World world, EntityPlayer player) {
+		player.addPotionEffect(new PotionEffect(Potion.digSpeed.getId(), 5, 10));
+		return super.onEaten(stack, world, player);
 	}
 
 }
--- main/java/com/deerangle/main/SchokoMod.java
+++ main/java/com/deerangle/main/SchokoMod.java
@@ -11,8 +11,8 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod {
+@Mod(modid = SchokoMod.MODID, version = SchokoMod.VERSION)
+public class SchokoMod {
 	public static final String NAME = "Schoko Mod";
 	public static final String MODID = "schokomod";
 	public static final String VERSION = "1.0";
