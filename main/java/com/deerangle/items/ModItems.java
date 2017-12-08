010030410923307827d9372a4bc8f770af68eb75
deerangle2 <ian.rehwinkel@kabelmail.de>
2017-12-08 14:53:30 +0100

    ok

--- main/java/com/deerangle/items/ModItems.java
+++ main/java/com/deerangle/items/ModItems.java
@@ -4,8 +4,39 @@ import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.item.Item;
 
 public class ModItems {
-	
+
+	public static Item schokoBarBlack;
+	public static Item schokoBarApple;
+	public static Item schokoBarCobble;
+	public static Item schokoBarCow;
+	public static Item schokoBarCreeper;
+	public static Item schokoBarCristmas;
+	public static Item schokoBarDoor;
+	public static Item schokoBarDrawing;
+	public static Item schokoBarEnder;
+	public static Item schokoBarFire;
+	public static Item schokoBarFireworks;
+	public static Item schokoBarFlower;
+	public static Item schokoBarFull;
+	public static Item schokoBarGlass;
+	public static Item schokoBarGold;
+	public static Item schokoBarHalloween;
+	public static Item schokoBarIlluminati;
+	public static Item schokoBarJoghurt;
+	public static Item schokoBarLite;
+	public static Item schokoBarLSD;
 	public static Item schokoBarNormal;
+	public static Item schokoBarNuts;
+	public static Item schokoBarPortal;
+	public static Item schokoBarQuartz;
+	public static Item schokoBarRedstone;
+	public static Item schokoBarRubiks;
+	public static Item schokoBarSlot;
+	public static Item schokoBarSmartie;
+	public static Item schokoBarSteve;
+	public static Item schokoBarWhite;
+	public static Item schokoBarWood;
+	public static Item schokoBarYouTube;
 	
 	public static void load(){
 		initialize();
