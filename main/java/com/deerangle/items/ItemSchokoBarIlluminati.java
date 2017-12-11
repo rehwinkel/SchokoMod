75e9cc97e4c7472da309af2cc19c22f1ee1ba599
deerangle2 <ian.rehwinkel@kabelmail.de>
2017-12-11 16:42:13 +0100

    26

--- main/java/com/deerangle/items/ItemSchokoBarIlluminati.java
+++ main/java/com/deerangle/items/ItemSchokoBarIlluminati.java
@@ -13,16 +13,16 @@ public class ItemSchokoBarIlluminati extends ItemSchokoBar {
 	public ItemSchokoBarIlluminati() {
 		super("illuminati");
 	}
-	
+
 	@Override
 	public ItemStack onEaten(ItemStack stack, World world, EntityPlayer player) {
 		world.playSoundAtEntity(player, SchokoMod.MODID + ":xfiles", 1, 1);
-		player.addPotionEffect(new PotionEffect(SchokoMod.schokoPotion.id, 30 * 20, 100));
-		player.addPotionEffect(new PotionEffect(SchokoMod.lsdPotion.id, 30 * 20, 0));
-		player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 30 * 20, 2));
-		player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, 30 * 20, 2));
-		player.addPotionEffect(new PotionEffect(Potion.blindness.id, 30 * 20, 2));
-		player.addPotionEffect(new PotionEffect(Potion.confusion.id, 30 * 20, 2));
+		player.addPotionEffect(new PotionEffect(SchokoMod.schokoPotion.id, 26 * 20, 100));
+		player.addPotionEffect(new PotionEffect(SchokoMod.lsdPotion.id, 26 * 20, 0));
+		player.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 26 * 20, 2));
+		player.addPotionEffect(new PotionEffect(Potion.digSlowdown.id, 26 * 20, 2));
+		player.addPotionEffect(new PotionEffect(Potion.blindness.id, 26 * 20, 2));
+		player.addPotionEffect(new PotionEffect(Potion.confusion.id, 26 * 20, 2));
 		player.addPotionEffect(new PotionEffect(Potion.invisibility.id, 26 * 20, 4));
 		return super.onEaten(stack, world, player);
 	}
