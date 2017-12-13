143ae2678fce9d78c067af7a57372a79298a4dbe
deerangle2 <ian.rehwinkel@kabelmail.de>
2017-12-13 17:40:06 +0100

    k

--- main/java/com/deerangle/main/WorldGenWeed.java
+++ main/java/com/deerangle/main/WorldGenWeed.java
@@ -20,52 +20,43 @@ public class WorldGenWeed extends WorldGenerator {
 
 	public boolean generate(World world, Random random, int x, int y, int z) {
 		if (world.getBlock(x, y - 1, z) == Blocks.grass || world.getBlock(x, y - 1, z) == Blocks.dirt) {
-			world.setBlock(x, y, z, ModBlocks.weedBush);
+			if(world.isAirBlock(x, y, z)){
+				world.setBlock(x, y, z, ModBlocks.weedBush);
+				world.setBlockMetadataWithNotify(x, y, z, 3, 2);
+			}
+		}
+		if (world.getBlock(x + 1, y - 1, z) == Blocks.grass || world.getBlock(x + 1, y - 1, z) == Blocks.dirt) {
+			if(world.isAirBlock(x + 1, y, z)){
+				if(Math.random() > 0.5){
+					world.setBlock(x + 1, y, z, ModBlocks.weedBush);
+					world.setBlockMetadataWithNotify(x + 1, y, z, 3, 2);
+				}
+			}
+		}
+		if (world.getBlock(x - 1, y - 1, z) == Blocks.grass || world.getBlock(x - 1, y - 1, z) == Blocks.dirt) {
+			if(world.isAirBlock(x - 1, y, z)){
+				if(Math.random() > 0.5){
+					world.setBlock(x - 1, y, z, ModBlocks.weedBush);
+					world.setBlockMetadataWithNotify(x - 1, y, z, 3, 2);
+				}
+			}
+		}
+		if (world.getBlock(x, y - 1, z + 1) == Blocks.grass || world.getBlock(x, y - 1, z + 1) == Blocks.dirt) {
+			if(world.isAirBlock(x, y, z + 1)){
+				if(Math.random() > 0.5){
+					world.setBlock(x, y, z + 1, ModBlocks.weedBush);
+					world.setBlockMetadataWithNotify(x, y, z + 1, 3, 2);
+				}
+			}
+		}
+		if (world.getBlock(x, y - 1, z - 1) == Blocks.grass || world.getBlock(x, y - 1, z - 1) == Blocks.dirt) {
+			if(world.isAirBlock(x, y, z - 1)){
+				if(Math.random() > 0.5){
+					world.setBlock(x, y, z - 1, ModBlocks.weedBush);
+					world.setBlockMetadataWithNotify(x, y, z - 1, 3, 2);
+				}
+			}
 		}
 		return true;
-//		float f = random.nextFloat() * (float) Math.PI;
-//		double d0 = (double) ((float) (x + 8) + MathHelper.sin(f) * (float) this.numberOfBlocks / 8.0F);
-//		double d1 = (double) ((float) (x + 8) - MathHelper.sin(f) * (float) this.numberOfBlocks / 8.0F);
-//		double d2 = (double) ((float) (z + 8) + MathHelper.cos(f) * (float) this.numberOfBlocks / 8.0F);
-//		double d3 = (double) ((float) (z + 8) - MathHelper.cos(f) * (float) this.numberOfBlocks / 8.0F);
-//		double d4 = (double) (y + random.nextInt(3) - 2);
-//		double d5 = (double) (y + random.nextInt(3) - 2);
-//
-//		for (int l = 0; l <= this.numberOfBlocks; ++l) {
-//			double d6 = d0 + (d1 - d0) * (double) l / (double) this.numberOfBlocks;
-//			double d7 = d4 + (d5 - d4) * (double) l / (double) this.numberOfBlocks;
-//			double d8 = d2 + (d3 - d2) * (double) l / (double) this.numberOfBlocks;
-//			double d9 = random.nextDouble() * (double) this.numberOfBlocks / 16.0D;
-//			double d10 = (double) (MathHelper.sin((float) l * (float) Math.PI / (float) this.numberOfBlocks) + 1.0F)
-//					* d9 + 1.0D;
-//			double d11 = (double) (MathHelper.sin((float) l * (float) Math.PI / (float) this.numberOfBlocks) + 1.0F)
-//					* d9 + 1.0D;
-//			int i1 = MathHelper.floor_double(d6 - d10 / 2.0D);
-//			int j1 = MathHelper.floor_double(d7 - d11 / 2.0D);
-//			int k1 = MathHelper.floor_double(d8 - d10 / 2.0D);
-//			int l1 = MathHelper.floor_double(d6 + d10 / 2.0D);
-//			int i2 = MathHelper.floor_double(d7 + d11 / 2.0D);
-//			int j2 = MathHelper.floor_double(d8 + d10 / 2.0D);
-//
-//			for (int k2 = i1; k2 <= l1; ++k2) {
-//				double d12 = ((double) k2 + 0.5D - d6) / (d10 / 2.0D);
-//
-//				if (d12 * d12 < 1.0D) {
-//					for (int l2 = j1; l2 <= i2; ++l2) {
-//						double d13 = ((double) l2 + 0.5D - d7) / (d11 / 2.0D);
-//
-//						if (d12 * d12 + d13 * d13 < 1.0D) {
-//							for (int i3 = k1; i3 <= j2; ++i3) {
-//								double d14 = ((double) i3 + 0.5D - d8) / (d10 / 2.0D);
-//
-//								if (d12 * d12 + d13 * d13 + d14 * d14 < 1.0D && world.getBlock(k2, l2, i3).isReplaceableOreGen(world, k2, l2, i3, replace)) {
-//									world.setBlock(k2, l2, i3, this.oreType, mineableBlockMeta, 2);
-//								}
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
 	}
 }
