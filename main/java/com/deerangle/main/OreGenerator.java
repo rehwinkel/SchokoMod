c2dda7bc3a22ae34bffe1925b023e39a3d3e6787
Creepsy <33872302+Creepsy@users.noreply.github.com>
2017-12-13 16:34:45 +0100

    added oregen

---
+++ main/java/com/deerangle/main/OreGenerator.java
@@ -0,0 +1,51 @@
+package com.deerangle.main;
+
+import java.util.Random;
+
+import com.deerangle.tile.ModBlocks;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.feature.WorldGenMinable;
+import net.minecraft.world.gen.feature.WorldGenerator;
+
+public class OreGenerator implements IWorldGenerator {
+
+	private WorldGenerator weedBushGen;
+
+	public OreGenerator() {
+		this.weedBushGen = new WorldGenMinable(ModBlocks.weedBush, 8);
+
+	}
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator,
+			IChunkProvider chunkProvider) {
+		switch (world.provider.dimensionId) {
+		case 0: // Overworld
+			this.runGenerator(this.weedBushGen, world, random, chunkX, chunkZ, 20, 0, 70);
+			break;
+		case -1: // Nether
+
+			break;
+		case 1: // End
+
+			break;
+		}
+	}
+
+	private void runGenerator(WorldGenerator generator, World world, Random rand, int chunk_X, int chunk_Z,
+			int chancesToSpawn, int minHeight, int maxHeight) {
+		if (minHeight < 0 || maxHeight > 256 || minHeight > maxHeight)
+			throw new IllegalArgumentException("Illegal Height Arguments for WorldGenerator");
+
+		int heightDiff = maxHeight - minHeight + 1;
+		for (int i = 0; i < chancesToSpawn; i++) {
+			int x = chunk_X * 16 + rand.nextInt(16);
+			int y = minHeight + rand.nextInt(heightDiff);
+			int z = chunk_Z * 16 + rand.nextInt(16);
+			generator.generate(world, rand, x, y, z);
+		}
+	}
+}
