1c991c13244170999c39074d76f4b1dfd897aaaf
deerangle2 <ian.rehwinkel@kabelmail.de>
2017-12-19 15:18:18 +0100

    k

--- main/java/com/deerangle/main/SchokoMod.java
+++ main/java/com/deerangle/main/SchokoMod.java
@@ -41,7 +41,7 @@ import net.minecraftforge.common.MinecraftForge;
 public class SchokoMod {
 	public static final String NAME = "Noah's Chocolate!";
 	public static final String MODID = "noahschocolate";
-	public static final String VERSION = "1.0.133";
+	public static final String VERSION = "1.0.136";
 
 	public static CreativeTabs bars = new CreativeTabs("schoko.bars") {
 		@Override
